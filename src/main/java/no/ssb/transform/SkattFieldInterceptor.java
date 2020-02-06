1e02d97dd6b002be852c36e4e07487d6d98fa32e
RuneLind <rulind@gmail.com>
2020-02-06 11:19:36 +0100

    Add more skatt generating rules

--- src/main/java/no/ssb/transform/SkattFieldInterceptor.java
+++ src/main/java/no/ssb/transform/SkattFieldInterceptor.java
@@ -4,10 +4,16 @@ import no.ssb.avro.convert.core.SchemaBuddy;
 import no.ssb.avro.generate.FieldInterceptor;
 import no.ssb.avro.generate.GeneratedField;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
 public class SkattFieldInterceptor extends FieldInterceptor {
 
-    long persIdNumber = 1_000_000_000;
-    long persIdNumberBarn = 2_000_000_000;
+    private final long persIdNumber = 1_000_000_000;
+    private final long persIdNumberBarn = 2_000_000_000;
+    private final LocalDateTime start = LocalDateTime.of(2019, 1, 1, 0, 0);
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
 
     @Override
     protected GeneratedField handleField(SchemaBuddy schema, int rowNum, int arrayElementNum) {
@@ -37,14 +43,63 @@ public class SkattFieldInterceptor extends FieldInterceptor {
                 return GeneratedField.fromString("NO");
             case "andelAvFribeloep":
                 return createRandom(100);
+            case "registreringsdato":
+                return GeneratedField.fromString(start.plusDays(rowNum).format(formatter));
+            case "pensjonstype":
+                return getPensionType(schema, rowNum, arrayElementNum);
+            case "pensjonsordningstype":
+                return getPensionSchemeType(schema, rowNum, arrayElementNum);
             default:
                 return GeneratedField.shouldBeGenerated();
         }
         return GeneratedField.shouldBeGenerated();
     }
 
+    GeneratedField getPensionType(SchemaBuddy schema, int rowNum, int arrayElementNum) {
+        final List<String> personTypes = List.of(
+                "Ektefelletillegg",
+                "ufoeretrygdFraFolketrygden",
+                "ufoereytelseFraIPAOgIPS",
+                "ufoereytelseFraUtlandet",
+                "annenPensjonFraUtlandet",
+                "supplerendeStoenad",
+                "alderspensjonFraIPAOgIPS",
+                "pensjonFraEOESTilsvarendeNorskAlderspensjonEllerAFP"
+        );
+
+        if (!schema.getParent().getName().equals("pensjonsinntekt")) {
+            return GeneratedField.shouldBeGenerated();
+        }
+        String personType = personTypes.get(random.nextInt(personTypes.size()));
+        return GeneratedField.fromString(personType);
+    }
+
+    GeneratedField getPensionSchemeType(SchemaBuddy schema, int rowNum, int arrayElementNum) {
+        final List<String> pensionSchemeTypes = List.of(
+                "individuellPensjonsavtale",
+                "skattefavorisertIndividuellSparingTilPensjon",
+                "pensjonsavtale"
+        );
+
+        if (!schema.getParent().getName().equals("individuellPensjonsordning")) {
+            return GeneratedField.shouldBeGenerated();
+        }
+        String pensionSchemeType = pensionSchemeTypes.get(random.nextInt(pensionSchemeTypes.size()));
+        return GeneratedField.fromString(pensionSchemeType);
+    }
+
     @Override
     public int getChildCount(int rowNum) {
         return random.nextInt(5);
+    }
+
+    @Override
+    public boolean skipRecord(SchemaBuddy schema, int rowNum, int level) {
+        return random.nextBoolean();
+    }
+
+    @Override
+    public boolean skipField(SchemaBuddy schema, int rowNum, int level) {
+        return random.nextBoolean();
     }
 }
--- src/test/java/no/ssb/transform/GenerateSyntheticDataSkattTest.java
+++ src/test/java/no/ssb/transform/GenerateSyntheticDataSkattTest.java
@@ -2,9 +2,11 @@ package no.ssb.transform;
 
 
 import no.ssb.avro.convert.core.DataElement;
+import no.ssb.avro.convert.core.SchemaAwareElement;
 import no.ssb.avro.convert.core.SchemaBuddy;
 import no.ssb.avro.generate.GenerateSyntheticData;
 import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericRecord;
 import org.junit.jupiter.api.Test;
 
 
@@ -25,15 +27,15 @@ class GenerateSyntheticDataSkattTest {
 
 //        List<String> results = new ArrayList<>();
         for (DataElement element : generateSyntheticData) {
-            DataElement id = element.findChildByName("personidentifikator");
-            System.out.println(id);
-            DataElement inntektsaar = element.findChildByName("inntektsaar");
-            System.out.println(inntektsaar);
-            DataElement skjermet = element.findChildByName("skjermet");
-            System.out.println(skjermet);
+//            DataElement id = element.findChildByName("personidentifikator");
+//            System.out.println(id);
+//            DataElement inntektsaar = element.findChildByName("inntektsaar");
+//            System.out.println(inntektsaar);
+//            DataElement skjermet = element.findChildByName("skjermet");
+//            System.out.println(skjermet);
             System.out.println(element.toString(true));
-//            GenericRecord genericRecord = SchemaAwareElement.toRecord(element, generateSyntheticData.getSchemaBuddy());
-//            System.out.println(genericRecord);
+            GenericRecord genericRecord = SchemaAwareElement.toRecord(element, generateSyntheticData.getSchemaBuddy());
+            System.out.println(genericRecord);
         }
 
     }
