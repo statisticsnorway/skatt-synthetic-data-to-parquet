0567d9bf17f1a831f04a6a66d750745596b1a56a
RuneLind <rulind@gmail.com>
2020-02-17 16:05:40 +0100

    Update test

--- src/test/java/no/ssb/transform/GenerateSyntheticDataSkattTest.java
+++ src/test/java/no/ssb/transform/GenerateSyntheticDataSkattTest.java
@@ -6,7 +6,6 @@ import no.ssb.avro.convert.core.SchemaAwareElement;
 import no.ssb.avro.convert.core.SchemaBuddy;
 import no.ssb.avro.generate.GenerateSyntheticData;
 import org.apache.avro.Schema;
-import org.apache.avro.generic.GenericRecord;
 import org.junit.jupiter.api.Test;
 
 
@@ -20,22 +19,45 @@ class GenerateSyntheticDataSkattTest {
     }
 
     @Test
+    void testConvertAllToMandatory() {
+        SchemaBuddy schemaBuddy = SchemaBuddy.parse(schema);
+
+        System.out.println(schemaBuddy.toString(true));
+    }
+
+    @Test
     void test() {
         SkattFieldInterceptor skattFieldInterceptor = new SkattFieldInterceptor();
 
-        GenerateSyntheticData generateSyntheticData = new GenerateSyntheticData(schema, 1, skattFieldInterceptor);
+        GenerateSyntheticData generateSyntheticData = new GenerateSyntheticData(schema, 1, skattFieldInterceptor, 0);
+
+
+        SchemaBuddy schemaBuddy = SchemaBuddy.parse(schema);
+//        System.out.println(schemaBuddy.toString(true));
+
+        SchemaBuddy personidentifikator = schemaBuddy.getChild("personidentifikator");
+//
+//        System.out.println(personidentifikator.isOptionalWithCheckOfAllChildren());
+
+
+//        System.out.println(schema.toString(true));
+//        if(true) return;
+
 
 //        List<String> results = new ArrayList<>();
         for (DataElement element : generateSyntheticData) {
+
 //            DataElement id = element.findChildByName("personidentifikator");
 //            System.out.println(id);
 //            DataElement inntektsaar = element.findChildByName("inntektsaar");
 //            System.out.println(inntektsaar);
 //            DataElement skjermet = element.findChildByName("skjermet");
 //            System.out.println(skjermet);
-            System.out.println(element.toString(true));
-            GenericRecord genericRecord = SchemaAwareElement.toRecord(element, generateSyntheticData.getSchemaBuddy());
-            System.out.println(genericRecord);
+//            System.out.println(element.toString(true));
+            SchemaAwareElement schemaAwareElement = SchemaAwareElement.toSchemaAwareElement(element, schemaBuddy);
+            System.out.println(schemaAwareElement.toString(true));
+//            GenericRecord genericRecord = SchemaAwareElement.toRecord(element, generateSyntheticData.getSchemaBuddy());
+//            System.out.println(genericRecord);
         }
 
     }
